cc2cea3388a5c10a8e581e155844347a78ebc424
Ali Alsaihati <ali.y.alsaihati@gmail.com>
2023-04-20 04:02:13 +0300

    first 4 methods and test class

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,18 +1,64 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Dictionary {
-    AVLTree<String> tree;
+    private AVLTree<String> tree;
     
-    public Dictionary(){
+    public Dictionary(){ // This constructor creates an empty AVL tree.
         this.tree = new AVLTree<>();
     }
     
-    public Dictionary(String string){
+    public Dictionary(String string){ // This constructor creates an AVL tree with a root containing the specified string.
         this.tree = new AVLTree<>();
-        tree.root = new BTNode<String>(string);
+        tree.insertAVL(string);
     }
     
-    public Dictionary(File file){
+    public Dictionary(File file){ // This constructor creates an AVL tree with nodes containing the words in a file.
         this.tree = new AVLTree<>();
+        try(Scanner fileScanner = new Scanner(file)){
+            while(fileScanner.hasNext()){
+                tree.insertAVL(fileScanner.next());
+            }
+            System.out.println("\ndictionary loaded successfully.");
+        }
+        catch(FileNotFoundException ex){
+            System.out.println(ex);
+        }
     }
+
+    public void addWord(String s) throws WordAlreadyExistsException{
+        if(!findWord(s)){ // The word is not in the dictionary.
+            tree.insertAVL(s);
+            System.out.println("\nword added successfully.");
+        }
+        else{ // The word is already in the dictionary
+            throw new WordAlreadyExistsException("The word is already in the dictionary.");
+        }
+    }
+
+    public boolean findWord(String s){
+        if(tree.search(s)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void deleteWord(String s) throws WordNotFoundException{
+        if(findWord(s)){ // The word is in the dictionary
+            tree.deleteAVL(s);
+            System.out.println("\nword deleted successfully.");
+        }
+        else{ // // The word is not in the dictionary
+            throw new WordNotFoundException("word not found.");
+        }
+    }
+
+    // public void saveFile( File? fileName? Dictionary object? ){ txt file? binary? 
+
+    // }
+
+
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,59 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TestClass {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception{
+        // Asking for the filename
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter filename> ");
+        String fileName = input.next();
+        
+        // creating the dictionary
+        Dictionary dictionary = new Dictionary(new File(fileName));
+
+        // Suggesting operations
+        while(true){ // keep asking the user choose to save the file or exit
+            System.out.println("Type the number of the operation you want to do:");
+            System.out.print("1. Adding a word into the dictionary.");
+            System.out.print("2. Deleting a word from the dictionary.");
+            System.out.print("3. Searching for a word in the dictionary.");
+            System.out.print("4. Searching for similar words to a specific word.");
+            System.out.print("5. Save.");
+            System.out.print("6. Exit.");
+            int option = input.nextInt();
+
+            if(option == 1){ // Adding a word
+
+            }
+            
+            else if(option == 2){ // Deleting a word
+
+            }
+
+            else if(option == 3){ // Searching for a word
+            
+            }
+
+            else if(option == 4){ // Searching for similar words
+
+            }
+
+            else if(option == 5){ // Save
+
+            }
+
+            else if(option == 6){ // Exit
+                ///
+                ///
+                ///
+                break;
+            }
+
+            else{ // wrong option
+
+            }
+        }
+
     }
 }
